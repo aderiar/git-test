9b0d19db77ed8bc830be154c725abc38a453f011
yijiexiu <1540692473@qq.com>
2021-08-04 16:49:42 +0800

    demo commit2

--- src/main/java/com/aderial/git/GitTest.java
+++ src/main/java/com/aderial/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git world!");
         System.out.println("hello git2 world!");
+        System.out.println("hello git3 world!");
     }
 }
