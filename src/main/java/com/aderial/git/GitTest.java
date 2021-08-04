af0c4cdf3ff7c5551dcb63b974477f4e9090c38d
yijiexiu <1540692473@qq.com>
2021-08-04 16:40:46 +0800

    second commit

--- src/main/java/com/aderial/git/GitTest.java
+++ src/main/java/com/aderial/git/GitTest.java
@@ -6,6 +6,7 @@ package com.aderial.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git world!");
     }
 }
