0b87f58e446049bdbc7302e74ddb5159880ab168
yijiexiu <1540692473@qq.com>
2021-08-04 16:39:21 +0800

    first commit

---
+++ src/main/java/com/aderial/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.aderial.git;
+
+/**
+ * @author aderiar
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
